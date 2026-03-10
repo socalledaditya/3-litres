df79791203fedb47e3b551eac88165efe819a666
socalledaditya <adityakumarspartan221@gmail.com>
2026-03-11 02:43:07 +0530

    finished creating Drink 3-Litres v1

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const waterLevel_box = document.querySelector(".waterLevel-box");
+  const waterContainer = document.querySelector(".container");
+  const water_qtys = document.querySelectorAll(".water-qty");
+  const targetWater = 3000;
+  function getWaterLevelData() {
+    return JSON.parse(localStorage.getItem("waterLevelData"));
+  }
+  function setWaterLevelData(waterLevelData) {
+    localStorage.setItem("waterLevelData", JSON.stringify(waterLevelData));
+  }
+  let waterLevelData = getWaterLevelData();
+  const today = new Date();
+  if (!waterLevelData || waterLevelData.date !== today.getDate()) {
+    waterLevelData = {
+      waterLevel: 0,
+      date: today.getDate(),
+      targetWater,
+    };
+    setWaterLevelData(waterLevelData);
+    waterLevel_box.style.height = `${(waterLevelData.waterLevel / targetWater) * 100}%`;
+  }
+  if (waterLevelData.waterLevel >= 3000) {
+    waterLevel_box.style.height = "100%";
+  }
+  waterContainer.addEventListener("click", () => {
+    let currentWaterLevel = waterLevelData.waterLevel;
+    if (currentWaterLevel <= 3000) {
+      currentWaterLevel = currentWaterLevel + 250;
+      waterLevelData.waterLevel = currentWaterLevel;
+      setWaterLevelData(waterLevelData);
+      waterLevel_box.style.height = `${(currentWaterLevel / 3000) * 100}%`;
+    }
+    if (currentWaterLevel >= 3000) {
+      waterLevel_box.style.height = "100%";
+    }
+
+    console.log(currentWaterLevel);
+  });
+
+  water_qtys.forEach((water_qty) => {
+    water_qty.addEventListener("click", (e) => {
+      const qtyValue = e.target.innerText.split("ml")[0];
+      let currentWaterLevel = waterLevelData.waterLevel;
+      if (currentWaterLevel <= 3000) {
+        currentWaterLevel = currentWaterLevel + Number(qtyValue);
+        waterLevelData.waterLevel = currentWaterLevel;
+        setWaterLevelData(waterLevelData);
+        waterLevel_box.style.height = `${(currentWaterLevel / 3000) * 100}%`;
+      }
+      if (currentWaterLevel >= 3000) {
+        waterLevel_box.style.height = "100%";
+      }
+      console.log(qtyValue);
+    });
+  });
+});
